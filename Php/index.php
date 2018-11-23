5f27756978968ec6a5fb99f291530e4fef4329a5
Sean Irwin <d00191063@student.dkit.ie>
2018-11-23 11:33:54 +0000

    Modal pop-up for register(no actual data insertion made)

--- Php/index.php
+++ Php/index.php
@@ -68,6 +68,38 @@
                 </div>
               </div>
             </div>
-        </div>           
+        </div> 
+        <div class="modal fade" id="modalRegisterForm" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+            <div class="modal-dialog" role="document">
+              <div class="modal-content">
+                <div class="modal-header text-center">
+                  <h4 class="modal-title w-100 font-weight-bold">Register</h4>
+                  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                  </button>
+                </div>
+                <div class="modal-body mx-3">
+                  
+                  <div class="md-form mb-5">
+                    <i class="fa fa-envelope prefix grey-text"></i>
+                    <input type="username" id="orangeForm-email" class="form-control validate" placeholder="Username">
+                  </div>
+                  <div class="md-form mb-5">
+                    <i class="fa fa-envelope prefix grey-text"></i>
+                    <input type="email" id="orangeForm-email" class="form-control validate" placeholder="Email">
+                  </div>
+
+                  <div class="md-form mb-4">
+                    <i class="fa fa-lock prefix grey-text"></i>
+                    <input type="password" id="orangeForm-pass" class="form-control validate" placeholder="Password">
+                  </div>
+
+                </div>
+                <div class="modal-footer d-flex justify-content-center">
+                  <button class="btn btn-deep-orange">Sign In</button>
+                </div>
+              </div>
+            </div>
+        </div>
     </body>
 </html>
